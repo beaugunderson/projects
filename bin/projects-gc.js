e1f95cdc84fb8da9285032e6dcbb115419bc79f8
Beau Gunderson <beau@beaugunderson.com>
2013-08-09 22:30:43 -0700

    Add dirty-compact, gc command, refactor utilities

--- bin/projects-gc.js
+++ bin/projects-gc.js
@@ -1,4 +1,28 @@
-// This will rely on dirty-compact, which I've started writing but which doesn't
-// yet exist.
+#!/usr/bin/env node
 
-//var compact = require('dirty-compact');
+exports.command = {
+  description: 'compact the projects database'
+};
+
+if (require.main !== module) {
+  return;
+}
+
+var compact = require('dirty-compact').compact;
+var util = require('util');
+
+var utilities = require('../lib/utilities.js');
+
+var backup = utilities.DATABASE_FILE + '.' + new Date().valueOf();
+
+console.log(util.format('Backing up %s to %s',
+  utilities.DATABASE_FILE, backup));
+
+compact(utilities.DATABASE_FILE, backup, function (err) {
+  if (err) {
+    return console.error(util.format('Error compacting %s: %s',
+      utilities.DATABASE_FILE, err));
+  }
+
+  console.log('Finished compacting', utilities.DATABASE_FILE);
+});
