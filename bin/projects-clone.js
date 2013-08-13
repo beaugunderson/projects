c2b212a61d31b7d14b54a6f00613cbd167c168e2
Beau Gunderson <beau@beaugunderson.com>
2013-08-13 01:37:17 -0700

    Add flag to clone all projects' repositories

--- bin/projects-clone.js
+++ bin/projects-clone.js
@@ -9,6 +9,9 @@ if (require.main !== module) {
   return;
 }
 
+var async = require('async');
+var chalk = require('chalk');
+var fs = require('fs');
 var path = require('path');
 var program = require('commander');
 var spawn = require('child_process').spawn;
@@ -18,23 +21,14 @@ var paths = require('../lib/paths.js');
 var storage = require('../lib/storage.js');
 var utilities = require('../lib/utilities.js');
 
+program.option('-a, --all', 'clone all projects with a repository');
 program.option('-d, --directory', 'the directory to clone into');
 
 program._name = 'clone';
 program.usage('<project>');
 program.parse(process.argv);
 
-var projectName = program.args[0];
-
-if (!projectName) {
-  console.error('Please specify a project.');
-
-  process.exit(1);
-}
-
-storage.setup(function () {
-  var project = storage.getProjectOrDie(projectName);
-
+function clone(project, cb) {
   var directory;
 
   directory = project.directory ||
@@ -47,13 +41,55 @@ storage.setup(function () {
     process.exit(1);
   }
 
-  // Store the directory we resolved for the project in its entry for use by
-  // other scripts
-  storage.updateProject(projectName, { directory: directory }, function () {
-    directory = utilities.expand(directory);
+  fs.exists(utilities.expand(directory), function (exists) {
+    if (exists) {
+      console.warn(chalk.yellow('The path "%s" already exists, skipping.'),
+        directory);
 
-    spawn('git',
-      ['clone', project.repository,  directory],
-      { stdio: 'inherit' }).on('close', process.exit);
+      return cb();
+    }
+
+    // Store the directory we resolved for the project in its entry for use by
+    // other scripts
+    storage.updateProject(project.name, { directory: directory }, function () {
+      directory = utilities.expand(directory);
+
+      spawn('git',
+        ['clone', project.repository,  directory],
+        { stdio: 'inherit' }).on('close', cb);
+    });
   });
+}
+
+storage.setup(function () {
+  if (program.all) {
+    if (program.directory) {
+      console.error('Error: --all and --directory are mutually exclusive.');
+
+      process.exit(1);
+    }
+
+    var projects = storage.query({ repository: { $has: true } },
+      { sortBy: function (project) { return project.name.toLowerCase(); } });
+
+    async.eachSeries(projects, function (project, cbEach) {
+      clone(project, cbEach);
+    }, function (err) {
+      if (err) {
+        console.error('Error cloning projects:', err);
+      }
+    });
+  } else {
+    var name = program.args[0];
+
+    if (!name) {
+      console.error('Please specify a project.');
+
+      process.exit(1);
+    }
+
+    var project = storage.getProjectOrDie(name);
+
+    clone(project, process.exit);
+  }
 });
