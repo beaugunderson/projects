f81dbddff1000502b6808dff144b19fe2494690e
Beau Gunderson <beau@beaugunderson.com>
2013-09-10 13:48:26 -0700

    Add usage

--- bin/projects.js
+++ bin/projects.js
@@ -4,4 +4,9 @@ var helmsman = require('helmsman');
 
 var program = helmsman({ usePath: true });
 
+program.on('--help', function () {
+  console.log('usage: projects [-h|--help] <command> ' +
+    '[--color auto|always|never] [<args>]');
+});
+
 program.parse();
