d4d430b92ab24d1c4e234fcfc9afc62db2844126
Beau Gunderson <beau@beaugunderson.com>
2014-11-24 18:02:41 -0800

    Rework projects-glob

--- bin/projects-glob.js
+++ bin/projects-glob.js
@@ -9,6 +9,8 @@ if (require.main !== module) {
   return;
 }
 
+require('log-buffer');
+
 var async = require('async');
 var fs = require('fs');
 var Glob = require('glob').Glob;
@@ -19,47 +21,75 @@ var utilities = require('../lib/utilities.js');
 
 var program = utilities.programDefaults('glob', '<command>');
 
-// XXX: Better way to word this?
-program.option('--dot', 'match files beginning with a . by default');
+program.option('--dot', 'match files beginning with a .');
 program.option('-d, --directories', 'only match directories');
 program.option('-f, --files', 'only match files');
 program.option('-n, --no-filter', 'don\'t filter .git, node_modules');
 
 program.parse(process.argv);
 
-var options = {
-  dot: program.dot
+if (program.files && program.directories) {
+  console.error('You can only specify either --files or --directories');
+
+  process.exit(1);
+}
+
+var globOptions = {
+  dot: program.dot,
+  nosort: true
 };
 
+if (program.files || program.directories) {
+  globOptions.stat = true;
+}
+
 var pattern = program.args.join(' ');
+
+var filterMatch = function (match) {
+  return match.indexOf('/node_modules/') > -1 ||
+         match.indexOf('/.git/') > -1;
+};
+
+if (program.noFilter) {
+  filterMatch = function () {
+    return false;
+  };
+}
+
+var statTest = 'isFile';
+
+if (program.directories) {
+  statTest = 'isDirectory';
+}
 
 storage.setup(function () {
   var projects = storage.allWithDirectory();
 
   async.eachSeries(projects, function (project, cbEach) {
-    var glob = new Glob(path.join(utilities.expand(project.directory),
-      pattern), options);
+    var globPattern = path.join(utilities.expand(project.directory), pattern);
+    var glob = new Glob(globPattern, globOptions);
 
-    glob.on('match', function (match) {
-      if (!program.noFilter && (/node_modules/.test(match) ||
-        /\/\.git\//.test(match))) {
-        return;
-      }
-
-      fs.stat(match, function (err, stats) {
-        // Sometimes there are broken symlinks
-        if (err && err.code === 'ENOENT') {
-          return console.log(utilities.contract(match));
-        } else if (err) {
-          throw err;
+    if (!program.files && !program.directories) {
+      glob.on('match', function (match) {
+        if (filterMatch(match)) {
+          return;
         }
 
-        if ((stats.isFile() && !program.directories) ||
-          (stats.isDirectory() && !program.files)) {
+        console.log(utilities.contract(match));
+      });
+    }
+
+    if (program.files || program.directories) {
+      glob.on('stat', function (match, stat) {
+        if (filterMatch(match)) {
+          return;
+        }
+
+        if (stat[statTest]()) {
           console.log(utilities.contract(match));
         }
       });
-    });
+    }
 
     glob.on('error', function (err) {
       console.log('Error globbing projects:', err);
